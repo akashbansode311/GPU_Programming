ab338a49694303f4fd556659eb72acc09d78e466
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2025-12-12 09:38:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,6 @@
 #SBATCH --error=cuda_%j.err              # Error log
 #SBATCH --partition=gpu                  # Partition
 #SBATCH --gres=gpu:1                     # Request 1 GPU
-#SBATCH --reservation=icts_gpuworkshop_nsmhrd  # Reservation name
 #SBATCH --time=00:30:00                  # Max time (hh:mm:ss)
 
 # Load CUDA module (adjust according to your system)
